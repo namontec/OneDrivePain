5669eb9ef715c2fb74e0f0b86270b3f38a6221ac
Victor Potasyev <vpotasyev@gmail.com>
2017-09-09 17:14:23 +0300

    finished

--- odPain.js
+++ odPain.js
@@ -1,2 +1,58 @@
+const fs = require('fs');
+const path = require('path');
 
-console.log("Test message");
+var maxPathLength = 260;
+
+//
+// Return array of files in folder and subfolders
+//
+function getFolders(localPath = './') {
+  var dirArray = [];
+  var fullPath = fs.realpathSync(localPath);
+
+  var files = fs.readdirSync(fullPath);
+
+  files.forEach(function (file) {
+    var pathFile = path.join(localPath, file, "/");
+    if ( fs.statSync(pathFile).isDirectory() ) {
+      // if file is directory recursively call getFolders()
+      dirArray = dirArray.concat( getFolders(pathFile) );
+    }
+    else {
+      dirArray.push( path.join(fullPath, file) );
+    }
+  })
+  return dirArray;
+}
+
+//
+// Return array of files which path is greater 
+//
+function isGreater(inArray, maxPathLength) {
+  var outArray = new Array();
+  
+  for (var i = 0; i < inArray.length; i++) {
+    var currentElement = inArray[i];
+    if (currentElement.length > maxPathLength) {
+      outArray.push(currentElement);
+    }
+  }
+
+  return outArray;
+}
+
+
+var folders = getFolders();
+var resultFolders = isGreater(folders, maxPathLength);
+
+if (resultFolders.length > 0) {
+  resultFolders.forEach(function(file) {
+    console.log(file);
+  })
+}
+else {
+  console.log("There are no files which names are greater", maxPathLength, "in", fs.realpathSync('./'));
+}
+
+
+
